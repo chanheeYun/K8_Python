bd744f2af1b7c35c2ea8addbcf47d5ebf0fd103a
chanheeYun <ych1782@naver.com>
2024-09-19 12:16:05 +0900

    Python Basic

--- pcc_3e-main/chapter_02/apostrophe.py
+++ pcc_3e-main/chapter_02/apostrophe.py
@@ -1,4 +1,9 @@
 n = 10
 print(n)
-n = "good"
-print(n.title())
+n = "  good nice today  "
+print("\n심볼 = " + n.title())
+print(f"\t심볼 =**{n.strip()}***")
+
+# java int a = 10, b = 20, c = 30;
+a,b,c = 10,20,30
+print(a)
